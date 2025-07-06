3f28e6331fc90bd9b634e77b6e3d73ec3dfc5600
Caleb Hester <naclcaleb@gmail.com>
2025-07-06 15:28:02 -0700

    Back to 16000

--- Python/main.py
+++ Python/main.py
@@ -33,7 +33,7 @@ print('Data read successfully. Processing data...')
 with wave.open('output.wav', 'wb') as wav_file:
     wav_file.setnchannels(1)
     wav_file.setsampwidth(2)  # 16-bit samples
-    wav_file.setframerate(8000)
+    wav_file.setframerate(16000)
     wav_file.writeframes(np_data.tobytes())
 
 
