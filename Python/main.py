b92fb2f112ecb2fbc556807b464d7ca1156a30b5
Caleb Hester <naclcaleb@gmail.com>
2025-07-06 14:55:38 -0700

    Test again

--- Python/main.py
+++ Python/main.py
@@ -19,7 +19,7 @@ print('Reading serial data...')
 
 data = serial.read(BYTES_TO_READ)
 d_type = np.dtype(np.uint16).newbyteorder('<')
-np_data = np.frombuffer(data, dtype=d_type)
+np_data = np.frombuffer(data, dtype=d_type).astype(np.int16)
 
 np_data = np_data - 2048
 
