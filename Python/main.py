e7a579f702a2d03df16708ff3b69f90db49453c6
Caleb Hester <naclcaleb@gmail.com>
2025-07-06 15:19:07 -0700

    blah

--- Python/main.py
+++ Python/main.py
@@ -19,6 +19,7 @@ time.sleep(5)
 print('Reading serial data...')
 
 data = serial.read(BYTES_TO_READ)
+print(f'Read {len(data)} bytes from serial port.')
 d_type = np.dtype(np.uint16).newbyteorder('<')
 np_data = np.frombuffer(data, dtype=d_type).astype(np.int16)
 
