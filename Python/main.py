ed65b79561be86f05b9f51ec62579d5dae27f848
Caleb Hester <naclcaleb@gmail.com>
2025-07-06 13:55:10 -0700

    use numpy frombuffer

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,6 @@
 from serial import Serial
 from serial.tools import list_ports
+import numpy as np
 
 BYTES_TO_READ = 20
 BAUD_RATE = 115200
@@ -15,12 +16,14 @@ serial = Serial(port=ports[selected_port].device, baudrate=BAUD_RATE, timeout=1)
 print('Reading serial data...')
 
 data = serial.read(BYTES_TO_READ)
+np_data = np.frombuffer(data, dtype=np.uint16)
 
 print('Number of bytes read:', len(data))
+print('Numpy format: ', len(np_data))
 
 with open('output.bin', 'wb') as f:
     f.write(data)
 
 serial.close()
 print('Serial port closed.')
-print('Done.')
+print('Done.') 
